6e99d01eb5f22819267edeec4b5356e2b0e1ebc7
Maycow Marques <melo.maycow@gmail.com>
2024-01-18 07:15:37 -0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.export = {
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+    extend: {
+      colors: {
+        primary: {
+          100: "#144217",
+          200: "#113514",
+          300: "#0A180B",
+        },
+        secondary: "#ECB800",
+      },
+    },
+  },
+};
